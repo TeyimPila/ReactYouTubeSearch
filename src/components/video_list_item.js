6864d3b7a7c76f54984125c0b6d7b5fd554399f4
Teyim Pila <teyim.pila@aun.edu.ng>
2018-01-01 10:47:12 +0100

    code cleanup

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -7,16 +7,17 @@ const VideoListItem = ({video, onVideoSelect}) => {
     return (
         <li onClick={() => onVideoSelect(video)} className="list-group-item">
             <div className="video-list media">
+
                 <div className="media-left">
                     <img className="media-object" src={imageUrl}/>
                 </div>
-
 
                 <div className="media-body">
                     <div className="media-heading">
                         {videoTitle}
                     </div>
                 </div>
+
             </div>
         </li>
     );
